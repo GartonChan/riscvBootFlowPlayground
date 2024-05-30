773329ce0a49b8baa8f54a6d20b465fb31a64385
GartonChan <gartonchan@gmail.com>
2024-05-30 12:11:00 +0800

    rename

--- boot_uboot-spl/run-qemu-riscv.sh
+++ boot_uboot-spl/run-qemu-riscv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-./qemu/build/qemu-system-riscv64 -smp 2 \
+qemu-system-riscv64 -smp 2 \
     -m 2G \
     -nographic \
     -machine virt \
@@ -8,4 +8,6 @@
     -device loader,file=./u-boot/u-boot.itb,addr=0x80200000 \
     -blockdev driver=file,filename=./disk.img,node-name=disk \
     -device virtio-blk-device,drive=disk \
+    -device virtio-net-device,netdev=net0\
+    -netdev user,id=net0,net=192.168.42.0/24,hostfwd=tcp:127.0.0.1:5555-:22\
     -dtb qemu.dtb \
